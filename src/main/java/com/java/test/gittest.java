6d8f636b480a8b31568d8cc64f7cef11d2c1612d
Xiangchong <Xiangchong@atguigu.com>
2021-07-03 20:26:11 +0800

    first commit

---
+++ src/main/java/com/java/test/gittest.java
@@ -0,0 +1,13 @@
+package com.java.test;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author zengtao
+ * @Time 2021-07-03 20:21
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!!");
+    }
+}
